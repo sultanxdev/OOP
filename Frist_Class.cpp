0d3e25bdc68eb752b92dfabb78153551d2da669f
sultanxdev <sultancodess@gmail.com>
2026-01-12 22:45:26 +0530

    Refactor code structure for improved readability and maintainability

--- Frist_Class.cpp
+++ Frist_Class.cpp
@@ -1,44 +1,47 @@
-#include<iostream>
-#include<string>
-using namespace std:
+#include <iostream>
+#include <string>
+using namespace std;
 
-class Teacher{
-
-  private:
-
- double salary;
+class Teacher {
+private:
+    double salary;
 
 public:
+    string name;
+    int age;
+    string department;
 
- string name;
+    // Change department
+    void changeDept(const string& newDept) {
+        department = newDept;
+    }
 
- int age;
+    // Set salary
+    void setSalary(double sal) {
+        salary = sal;
+    }
 
- string department;
-
- // member function
-
- function change_dept(String change_dept)
- {
-  department=change_dept;
-
- }
-
- void function setsal(double sal){
-
-  salary= sal
-
- }
- void function getsal(){
-  return salary
- }
-
-
-
-
+    // Get salary
+    double getSalary() const {
+        return salary;
+    }
 };
-int main(){
-  Teacher t1;
-  t.name="sultan"
-  cout<<t1.name;
+
+int main() {
+    Teacher t1;
+
+    t1.name = "Sultan";
+    t1.age = 22;
+    t1.changeDept("Computer Science");
+    t1.setSalary(50000);
+
+    cout << "Name: " << t1.name << endl;
+    cout << "Age: " << t1.age << endl;
+    cout << "Department: " << t1.department << endl;
+    cout << "Salary: " << t1.getSalary() << endl;
+    cout<<"enter a number";
+    int a;
+    cin>>a;
+
+    return 0;
 }
